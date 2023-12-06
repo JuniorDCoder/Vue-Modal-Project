3fb81dfce95eca6a2313a459100e1e4e38da4211
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-12-06 21:05:25 +0100

    feat: Passing data and methods as props to components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <br>
+  <button @click="ToggleClick">Click Me</button>
+  <Modal v-if="isClicked" :isClicked="isClicked" :ToggleClick="ToggleClick"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data(){
+    return {
+      title: "My First Vue App :)",
+      isClicked: false,
+    }
+  },
+  methods: {
+    ToggleClick(){
+      this.isClicked = !this.isClicked
+    }
   }
 }
 </script>
@@ -22,5 +35,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>Modal Title</h1>
+            <p>Modal Content</p>
+            <button @click="ToggleClick">Cancel</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        isClicked: Boolean,
+        ToggleClick: {
+            Function, 
+            required: true
+        }
+    },
+    methods: {
+        hideModal(){
+            this.isClicked = !this.isClicked
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 200px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+</style>
